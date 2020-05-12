00b415508d745d08edc8993c200c53734a0742f3
game1095 <taweesak.in@gmail.com>
2020-05-12 16:54:58 +0700

    delete bag and search outbound

--- app/controllers/outbounds_controller.rb
+++ app/controllers/outbounds_controller.rb
@@ -1,6 +1,7 @@
 class OutboundsController < ApplicationController
   def index
-    @outbounds = Outbound.all
+    # @outbounds = Outbound.all
+    @outbounds = Outbound.search(params[:search])
   end
 
   def new
@@ -30,7 +31,7 @@ class OutboundsController < ApplicationController
       end
     end
   end
-
+  
   private
     def outbound_params
       params.require(:outbound).permit(:origin , :destination , :sent_date , :strap , :car , :outbound_number)
--- app/models/outbound.rb
+++ app/models/outbound.rb
@@ -1,4 +1,11 @@
 class Outbound < ApplicationRecord
   has_many :bags
-  
+
+  def self.search(search)
+  	if search.blank?
+  		Outbound.all
+  	else
+  		Outbound.where('outbound_number LIKE ?', "%#{search}%")
+  	end
+  end
 end
