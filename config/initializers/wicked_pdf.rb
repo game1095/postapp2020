116133b00b0292752d1a6849deca0ea49999bd6c
game1095 <taweesak.in@gmail.com>
2020-05-21 14:39:50 +0700

    fix pdf css and layout

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -19,16 +19,4 @@ WickedPdf.config = {
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
 }
-if Rails.env.production?
-  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-else
-  # Linux (check your processor for Intel x86 or AMD x64)
-  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
-  # OS X
-  # wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
-  # Windows
-  # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
-end
 
-WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
